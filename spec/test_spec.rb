e51db43fb192ae0ee6210dc66ca6df46e326003e
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-12-01 14:13:48 +0000

    [Finish]Rspecを使ってテストを書いてみよう

---
+++ spec/test_spec.rb
@@ -0,0 +1,14 @@
+describe '四則演算' do
+  context '足し算' do
+    it '1+1は2になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+  
+  context '足し算' do
+    it '2+2は4になる' do
+      expect(2 + 2).to eq 4
+    end
+  end
+end
+
